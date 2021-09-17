c54f89e3fd940616ed5c534b6861f7a107729d95
augusto-cesar-dsr <augusto_cesar.cc@outlook.com>
2021-09-17 14:50:30 -0300

    melhorias e bug fix

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,10 +1,24 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  
   def index
     @admins = Admin.all
   end
 
   def edit
     @admin = Admin.find(params[:id])
+  end
+
+  def update
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+    @admin = Admin.find(params[:id])
+    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+    if @admin.update(params_admin)
+      redirect_to admins_backoffice_admins_path, notice: "The administrator was updated!"
+    else
+      render :edit
+    end
   end
 
 end
