4b5d82016ce7fa1fb7c53f5d2308cb9ac7fcc8c8
augusto-cesar-dsr <augusto_cesar.cc@outlook.com>
2021-09-21 15:30:53 -0300

    uping subjects

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
  namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
     resources :admins
+    resources :subjects
   end
   
   devise_for :users
