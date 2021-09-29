26b4fa6223394cf3ef710c5c7bb44d619e56cb6c
augusto-cesar-dsr <augusto_cesar.cc@outlook.com>
2021-09-29 17:19:02 -0300

    crianto filtro por assunto

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,12 @@
 module SiteHelper
+  def msg_system
+    case params[:action]
+    when 'index'
+      "Last questions..."
+    when 'questions'
+      "Results for the term \"#{params[:term]}\"..."
+    when 'subject'
+      "Show questions for the subject \"#{params[:subject_id]}\"..."
+    end
+  end
 end
