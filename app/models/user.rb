b7d36576cea73a6ed0175a0b848b4758b9372828
augusto-cesar-dsr <augusto_cesar.cc@outlook.com>
2021-10-06 14:09:18 -0300

    user profile add

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :user_profile
+  
   # Validations
   validates :first_name, presence: true, length: { minimum: 3 }, on: :update
 
