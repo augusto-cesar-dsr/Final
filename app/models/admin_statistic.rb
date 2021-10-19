c1091ab90475b1c47981d42c9fed02323f510842
augusto-cesar-dsr <augusto_cesar.cc@outlook.com>
2021-10-19 11:36:24 -0300

    updating global methods

--- app/models/admin_statistic.rb
+++ app/models/admin_statistic.rb
@@ -3,6 +3,15 @@ class AdminStatistic < ApplicationRecord
     total_users: "TOTAL_USERS",
     total_questions: "TOTAL_QUESTIONS"
   }
+  
+  # scopes
+  scope :total_users, ->{
+    find_by_event(EVENTS[:total_users]).value
+  }
+  scope :total_questions, ->{
+    find_by_event(EVENTS[:total_questions]).value
+  }
+  # class methods
   def self.set_event(event)
     statistic = AdminStatistic.find_or_create_by(event: event)
     statistic.value += 1
